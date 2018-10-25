ac22fa14e5e4758b2aabd6a6124fe169459b1e74
Gregory Werbin <gregory.werbin@chubb.com>
2018-10-25 00:38:16 -0400

    Update authorship and copyright

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='words_to_number',
     version='1.0',
     description='Convert English text representation of numbers to numerical representation',
-    author='Greg Werbin and Rocketrip',
-    author_email='greg@rocketrip.com',
+    author='Greg Werbin',
+    author_email='outthere@me.gregwerbin.com',
     license='MIT',
-    url='https://github.com/greg-rocketrip/word_to_number',
-    packages=['words_to_number']
+    url='https://github.com/gwerbin/word_to_number',
+    packages=find_packages(exclude=['test'])
 )
